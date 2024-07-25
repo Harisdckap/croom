445af5a48eacef7b5309d56e1aec686f3c9557a5
prakashr2004 <prakashrdckap@gmail.com>
2024-07-25 16:46:57 +0530

    add_PG file name change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 // import { RoommateForm } from './components/Add_RoommateForm/RoommateForm'
-import { Add_PG } from './components/Add_PG/add_PG'
+import { Add_PG } from './components/Add_PG/Add_PG'
 // import { Add_Reuirement } from './components/Add_Requirment_Page/Add_Reuirement'
 
 
